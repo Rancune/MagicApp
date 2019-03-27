7722a20405f15abd6ee6cdd761707cd1c4d84d6c
Rancune <pitet.brian@gmail.com>
2019-03-27 12:31:58 +0100

    Add Initial Unity Files

---
+++ Assets/Scripts/LifePoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePoints : MonoBehaviour
+{
+    public int startpoint;
+
+    private GameObject child;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //child = this.GetComponentInChildren(typeof());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
