adcef33ac723fe7fb20329da18fe304cf56549b0
Rancune <pitet.brian@gmail.com>
2019-04-29 14:15:38 +0200

    Modif Build  to Android

---
+++ Assets/- Scripts/ScoreTracking.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class ScoreTracking : MonoBehaviour
+{
+  static public string[,] scoreArray;
+ // List<Word> words = new List<Word>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Init du tableau avec les noms des participants
+       // void DisplayArray(string[] initArray) => Console.WriteLine(string.Join(" ", arr));
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void InitScore(string[] initArray)
+    {
+       /*for (int i = 0; i <= initArray.GetLength; i++) 
+       {
+            scoreArray [0,i] = initArray[i];
+            scoreArray [1,i] = "0";
+       }*/
+
+      // words.Add(new Word(word));
+       //words.ToArray();
+    }
+}
+
+
